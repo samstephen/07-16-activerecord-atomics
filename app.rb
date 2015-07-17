bb6ca55862e166b6c3f1901851fe4ed980f60242
Sam Stephen <samueljstephen@gmail.com>
2015-07-17 14:07:31 -0500

    add production database settings

--- app.rb
+++ app.rb
@@ -15,6 +15,20 @@ configure :development do
   ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'database.db')
 end
 
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
+end
+
+
 # So that ActiveRecord explains the SQL it's running in the logs.
 ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
 
